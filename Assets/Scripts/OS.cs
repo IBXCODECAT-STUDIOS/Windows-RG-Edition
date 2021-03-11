efb2acaf6122bb0d337df0ec1f281fff29ddc7db
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-03-11 14:57:10 -0600

    Clippy Be Talkin

---
+++ Assets/Scripts/OS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public class OS : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    private bool active;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            active = !active;
+            MediaPlayer(active);
+        }
+    }
+
+    public void MediaPlayer(bool enabled)
+    {
+        if(enabled)
+        {
+            videoPlayer.gameObject.SetActive(true);
+            videoPlayer.Play();
+        }
+        else
+        {
+            videoPlayer.Stop();
+            videoPlayer.gameObject.SetActive(false);
+        }
+    }
+}
