8fa0f161d9bf12e5b00c5d411de9a9607e01e903
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-03-10 20:12:34 -0600

    Clippy Is Somewhat Functional

---
+++ Assets/Scripts/Clippy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Clippy : MonoBehaviour
+{
+    public Text bubbleText;
+    public Animator clippyAnim;
+    public int clippyDelay;
+
+    public string startMsg;
+    public string[] messages;
+
+    private string clippyToggleName = "show";
+    private bool clippyClicked;
+
+
+    public void OnClippyClick()
+    {
+        clippyClicked = true;
+    }
+
+    IEnumerator Start()
+    {
+        clippyAnim.SetBool(clippyToggleName, true);
+        bubbleText.text = startMsg;
+
+        while(true)
+        {
+            if(clippyClicked)
+            {
+                clippyAnim.SetBool(clippyToggleName, false); //Hide Clippy
+                yield return new WaitForSecondsRealtime(clippyDelay); //Wait
+                clippyAnim.SetBool(clippyToggleName, true); //Show Clippy
+
+                bubbleText.text = messages[Random.Range(0, messages.Length)]; //Choose random message.
+
+                clippyClicked = false; //Clippy no longer clicked
+            }
+            else
+            {
+                yield return new WaitForEndOfFrame(); //Wait for next frame
+            }
+        }
+    }
+}
