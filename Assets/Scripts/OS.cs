84252dad70128e18e34d7bc45c58e7cbedf13e84
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-03-12 20:22:35 -0600

    Restart And Shutdown Functionality Added

--- Assets/Scripts/OS.cs
+++ Assets/Scripts/OS.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
 public class OS : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public GameObject startMenu;
+    public AudioSource shutdownSound;
+    public AudioSource bootSound;
+    public GameObject logoOverlay;
 
     private bool videoActive;
     private bool startActive;
@@ -38,5 +43,27 @@ public class OS : MonoBehaviour
             videoPlayer.Stop();
             videoPlayer.gameObject.SetActive(false);
         }
+    }
+
+    public void Shutdown() { StartCoroutine(ShutdownRoutine()); }
+    public void Restart() { StartCoroutine(RestartRoutine()); }
+
+    IEnumerator ShutdownRoutine()
+    {
+        shutdownSound.Play();
+        logoOverlay.SetActive(true);
+        yield return new WaitForSecondsRealtime(2.5f);
+        Debug.Log("Quitting Application");
+        Application.Quit();
+    }
+
+    IEnumerator RestartRoutine()
+    {
+        shutdownSound.Play();
+        logoOverlay.SetActive(true);
+        yield return new WaitForSecondsRealtime(2.5f);
+        Debug.Log("Restarting");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Restarted");
     }
 }
